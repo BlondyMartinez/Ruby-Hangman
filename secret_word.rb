ce0d2f24737b8e80c92a72e22d0bc4df8500cfe4
BlondyMartinez <blondymm97@gmail.com>
2024-02-25 20:13:40 +0100

    changed initialise to initialize

--- secret_word.rb
+++ secret_word.rb
@@ -1,7 +1,7 @@
 class SecretWord
-    def initialise
-        data = File.read("./google-10000-english-no-swears.txt").split
-        data.select { |word| word.length >= 5 && word.length <= 12 }
-        data.sample
+    def initialize
+        data = File.read("google-10000-english-no-swears.txt").split
+        words = data.select { |word| word.length >= 5 && word.length <= 12 }
+        words.sample
     end
 end
