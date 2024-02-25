34bb983fb1709273919ea825ffd1430620913a55
BlondyMartinez <blondymm97@gmail.com>
2024-02-26 00:54:02 +0100

    made word attr_accessor

--- secret_word.rb
+++ secret_word.rb
@@ -1,4 +1,6 @@
 class SecretWord
+    attr_accessor :word
+
     def initialize
         data = File.read("google-10000-english-no-swears.txt").split
         @words = data.select { |word| word.length >= 5 && word.length <= 12 }
