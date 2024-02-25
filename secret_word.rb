d5f82737b8655a349d5baf9432bb0e1aa32084a5
BlondyMartinez <blondymm97@gmail.com>
2024-02-25 20:32:43 +0100

    changed secret_word structure

--- secret_word.rb
+++ secret_word.rb
@@ -1,7 +1,10 @@
 class SecretWord
     def initialize
         data = File.read("google-10000-english-no-swears.txt").split
-        words = data.select { |word| word.length >= 5 && word.length <= 12 }
-        words.sample
+        @words = data.select { |word| word.length >= 5 && word.length <= 12 }
+    end
+
+    def random_word
+        @words.sample
     end
 end
